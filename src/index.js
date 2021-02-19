d4993f5f1442835c0c18287235330cf629be20da
sylva-code <78614180+sylva-code@users.noreply.github.com>
2021-02-19 00:33:08 +0000

    Updated hourly and daily forecast

--- src/index.js
+++ src/index.js
@@ -27,7 +27,36 @@ function formatDay() {
   return `${day} ${currentTime}`;
 }
 
+function formatHours(timestamp) {
+  now = new Date(timestamp);
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
 
+  return `${hours}:${minutes}`;
+}
+
+function formatWeekday(timestamp) {
+  now = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let day = days[now.getDay()];
+
+  return `${day}`;
+}
 
   
 function showCity(event) {
@@ -43,18 +72,60 @@ function searchCity(city) {
 let apiKey = "d231d8f3710943f81168975979ce7164";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(url).then(currentWeather);
+
+url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(url).then(showForecast);
 }
 
 function currentWeather(response) { 
-  console.log(response);
+  //console.log(response);
   document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector("#current-weather-emoji").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#current-weather-icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#current-weather-icon").setAttribute("alt", response.data.weather[0].description);
   document.querySelector("#current-conditions").innerHTML = response.data.weather[0].main;
   document.querySelector("#current-temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed * 3.6);
+
+  let cityLatitude = response.data.coord.lat;
+  let cityLongitude = response.data.coord.lon;
+  apiKey = "d231d8f3710943f81168975979ce7164";
+  url = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLatitude}&lon=${cityLongitude}&exclude=minutely,alerts&appid=${apiKey}&units=metric`
+  axios.get(url).then(showDailyForecast);
 } 
+
+function showForecast(response) {
+  //console.log(response.data);
+  document.querySelector("#forecast-short").innerHTML = null;
+
+  for (let index = 0; index < 3; index++) {
+    let forecast = response.data.list[index];
+    document.querySelector("#forecast-short").innerHTML += `
+    <div class="col-2">
+      ${formatHours(forecast.dt * 1000)} <br />
+      <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"> <br />
+      ${Math.round(forecast.main.temp)}°
+    </div>
+    `; 
+  }
+}
+
+function showDailyForecast(response) {
+  //console.log(response.data.daily);
+  document.querySelector("#forecast-long").innerHTML = null;
+
+  for (let index = 1; index < 6; index++) {
+    let dailyForecast = response.data.daily[index];
+    document.querySelector("#forecast-long").innerHTML += `
+    <div class="col">
+      ${formatWeekday(dailyForecast.dt * 1000)} <br />
+      <img src="http://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png"> <br />
+      ${Math.round(dailyForecast.temp.max)}°/${Math.round(dailyForecast.temp.min)}°
+    </div>
+    `; 
+  }
+}
 
 searchCity("London");
 
